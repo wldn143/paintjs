fddab24c6e8dd4030bea2b8f05e9095c99b042c5
wldn143 <wldn143@gmail.com>
2022-02-07 14:44:26 +0900

    save the image

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors =document.getElementsByClassName("jsColor");
 const range =document.getElementById("jsRange");
 const mode =document.getElementById("jsMode");
+const saveBtn =document.getElementById("jsSave");
 
 const INITIAL_COLOR="#2c2c2c";
 const CANVAS_SIZE=700;
@@ -68,12 +69,26 @@ function handleCanvasClick(){
     }
 }
 
+function handleCM(event) {
+    event.preventDefault();
+  }
+  
+  function handleSaveClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    link.click();
+  }
+
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
     canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+
 
 }
 
@@ -88,3 +103,8 @@ if(range){
 if(mode){
     mode.addEventListener("click",handleModeClick);
 }
+
+if (saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
+  }
+  
